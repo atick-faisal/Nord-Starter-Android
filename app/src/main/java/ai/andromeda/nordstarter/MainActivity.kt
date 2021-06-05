101f0c05d199ef84f810f81e0e843d68c0bafc40
Atick Faisal <atickfaisal@gmail.com>
2021-06-06 05:25:23 +0600

    Add app db injecttion in MainActivity.kt for testing

--- app/src/main/java/ai/andromeda/nordstarter/MainActivity.kt
+++ app/src/main/java/ai/andromeda/nordstarter/MainActivity.kt
@@ -1,11 +1,21 @@
 package ai.andromeda.nordstarter
 
+import ai.andromeda.nordstarter.database.room.AppDatabase
+import ai.andromeda.nordstarter.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var appDatabase: AppDatabase
+    private lateinit var mainActivityUi: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityUi = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityUi.root)
     }
 }
