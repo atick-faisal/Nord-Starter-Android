0555ee386254176ee33f2b2e102e8df99b0780e7
Atick Faisal <atickfaisal@gmail.com>
2021-07-08 09:27:47 +0600

    Add nav drawer

--- app/src/main/java/ai/andromeda/nordstarter/MainActivity.kt
+++ app/src/main/java/ai/andromeda/nordstarter/MainActivity.kt
@@ -7,26 +7,50 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainActivityUi: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainActivityUi = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainActivityUi.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.apply {
+            setContentView(root)
+            setSupportActionBar(topAppBar)
+            navController = (
+                    supportFragmentManager
+                        .findFragmentById(navHostFragment.id)
+                            as NavHostFragment
+                    ).navController
+
+            setupWithNavController(navView, navController)
+            setupActionBarWithNavController(navController, root)
+        }
 
         viewModel.items.observe(this, { result ->
-            when(result) {
+            when (result) {
                 is Resource.Loading -> Log.i(LOG_TAG, "LOADING ... ")
                 is Resource.Success -> Log.i(LOG_TAG, "SUCCESS ... " + result.data)
                 is Resource.Error -> Log.i(LOG_TAG, "ERROR ... " + result.error)
             }
         })
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(navController, binding.root)
     }
 }
