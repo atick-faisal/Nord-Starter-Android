15aaec3f12a629ce8bd9f94adff45be186549f01
Atick Faisal <atickfaisal@gmail.com>
2021-07-14 05:05:58 +0600

    Add Event wrapper

---
+++ app/src/main/java/ai/andromeda/nordstarter/utils/Event.kt
@@ -0,0 +1,20 @@
+package ai.andromeda.nordstarter.utils
+
+// Event wrapper for Single Events
+// Details here: https://medium.com/androiddevelopers/livedata-with-snackbar-navigation-and-other-events-the-singleliveevent-case-ac2622673150
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+}
